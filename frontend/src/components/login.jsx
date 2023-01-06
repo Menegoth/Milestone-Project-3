7b2872cc3f594b1efc4c356165cfa4b4c1388049
Ryan McGowan <vitomaserati@gmail.com>
2023-01-05 20:29:08 -0800

    add onclick to change button on login form to change from login to signup form

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Box,Typography,TextField,Button} from '@mui/material';
 
 const Login = () => {
+  const [isSignup, setIsSignup]=useState(false)
   return (
     <div>
       <form>
         <Box display="flex" flexDirection={'column'} alignItems="center" justifyContent={"center" }boxShadow="10px 10px 20px #ccc" padding={3} margin='auto' marginTop={5} borderRadius={5} maxWidth={400}>
           <Typography padding={3} textAlign="center" variant="h2">Login</Typography>
-          <TextField margin="normal" placeholder='Name'/>
+          {isSignup && <TextField margin="normal" placeholder='Name'/>}
           <TextField margin="normal" placeholder='Email' type={'email'}/>
           <TextField margin="normal" placeholder='Password' type={'password'}/>
           <Button sx={{borderRadius:3, marginTop:3}} variant="contained" color='warning' >Submit</Button>
-          <Button sx={{borderRadius:3, marginTop:3}} >Change to Signup</Button>
+          <Button onClick={()=>setIsSignup(!isSignup)} sx={{borderRadius:3, marginTop:3}} >Change to {isSignup ? "Login" : "Signup"}</Button>
         </Box>
       </form>
     </div>
