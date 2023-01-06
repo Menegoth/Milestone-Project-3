1a037ffac156f3db8a2ba09d59be3fab056a7525
Ryan McGowan <vitomaserati@gmail.com>
2023-01-05 20:49:38 -0800

    set inputs for login form and onclick for submit button

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -2,16 +2,31 @@ import React,{useState} from 'react';
 import {Box,Typography,TextField,Button} from '@mui/material';
 
 const Login = () => {
-  const [isSignup, setIsSignup]=useState(false)
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup]=useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputs);
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Box display="flex" flexDirection={'column'} alignItems="center" justifyContent={"center" }boxShadow="10px 10px 20px #ccc" padding={3} margin='auto' marginTop={5} borderRadius={5} maxWidth={400}>
           <Typography padding={3} textAlign="center" variant="h2">{isSignup ? "Signup" : "Login"}</Typography>
-          {isSignup && <TextField margin="normal" placeholder='Name'/>}
-          <TextField margin="normal" placeholder='Email' type={'email'}/>
-          <TextField margin="normal" placeholder='Password' type={'password'}/>
-          <Button sx={{borderRadius:3, marginTop:3}} variant="contained" color='warning' >Submit</Button>
+          {isSignup && <TextField name="name" onChange={handleChange} value={inputs.name} margin="normal" placeholder='Name'/>}
+          <TextField name="email" onChange={handleChange} value={inputs.email} margin="normal" placeholder='Email' type={'email'}/>
+          <TextField name="password" onChange={handleChange} value={inputs.password} margin="normal" placeholder='Password' type={'password'}/>
+          <Button type='submit' sx={{borderRadius:3, marginTop:3}} variant="contained" color='warning' >Submit</Button>
           <Button onClick={()=>setIsSignup(!isSignup)} sx={{borderRadius:3, marginTop:3}} >Change to {isSignup ? "Login" : "Signup"}</Button>
         </Box>
       </form>
