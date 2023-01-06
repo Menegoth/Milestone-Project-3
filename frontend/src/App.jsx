a7569a31844401f69d4412a1c52abe6adf0b7e44
Ryan McGowan <vitomaserati@gmail.com>
2023-01-05 19:52:07 -0800

    create isLoggedIn slice for header signup/signin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,11 @@ import Login from './components/Login'
 import Home from './components/Home'
 import Trips from './components/Trips';
 import TripDetail from './components/TripDetail';
-
+import {useSelector} from 'react-redux'
 
 function App() {
+  const isLoggedIn = useSelector((state)=> state.isLoggedIn);
+  console.log(isLoggedIn);
   return <React.Fragment>
     <header>
       <Header/>
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {AppBar, Button, Toolbar, Typography,Box, } from '@mui/material'
 import { Link } from "react-router-dom";
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+  const isLoggedIn = useSelector((state)=> state.isLoggedIn);
   return <AppBar position='sticky' sx={{height:85, background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(11,75,200,1) 27%, rgba(214,8,8,1) 52%, rgba(5,17,232,1) 73%, rgba(8,8,8,1) 100%);"}}>
       <Toolbar>
         <Typography variant="h2" sx={{marginLeft:1}} >ManCations</Typography>
         <Box display="flex" marginLeft="auto">
-          <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Login</Button>
-          <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Signup</Button>
-          <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Logout</Button>
+          {!isLoggedIn && <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Login</Button>}
+          {!isLoggedIn && <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Signup</Button>}
+          {isLoggedIn && <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Logout</Button>}
         </Box>
       </Toolbar>
     </AppBar>
