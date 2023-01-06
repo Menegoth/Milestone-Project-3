abe2deee2e1321a2de76f66d52dfa11b50ec17a6
Ryan McGowan <vitomaserati@gmail.com>
2023-01-05 18:51:16 -0800

    add link components to login/logout buttons

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import {AppBar, Button, Toolbar, Typography,Box} from '@mui/material'
+import {AppBar, Button, Toolbar, Typography,Box, } from '@mui/material'
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <AppBar position='sticky' sx={{background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(11,75,200,1) 27%, rgba(214,8,8,1) 52%, rgba(5,17,232,1) 73%, rgba(8,8,8,1) 100%);"}}>
+  return <AppBar position='sticky' sx={{height:85, background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(11,75,200,1) 27%, rgba(214,8,8,1) 52%, rgba(5,17,232,1) 73%, rgba(8,8,8,1) 100%);"}}>
       <Toolbar>
-        <Typography variant="h4" >ManCations</Typography>
+        <Typography variant="h2" sx={{marginLeft:1}} >ManCations</Typography>
         <Box display="flex" marginLeft="auto">
-          <Button variant='contained' sx={{margin:1, borderRadius:10}} color="warning">Login</Button>
-          <Button variant='contained' sx={{margin:1, borderRadius:10}} color="warning">Signup</Button>
+          <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Login</Button>
+          <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Signup</Button>
+          <Button variant='contained' LinkComponent={Link}to="/Login" sx={{margin:1, borderRadius:10}} color="warning">Logout</Button>
         </Box>
       </Toolbar>
     </AppBar>
 }
 
 export default Header
+
+
+                
