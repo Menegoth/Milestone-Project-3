0c7692087ed5da232b485e90b97940c8271d5376
Ryan McGowan <vitomaserati@gmail.com>
2023-01-05 20:17:24 -0800

    create login form and basic styling

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Box,Typography,TextField,Button} from '@mui/material';
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div>
+      <form>
+        <Box display="flex" flexDirection={'column'} alignItems="center" justifyContent={"center" }boxShadow="10px 10px 20px #ccc" padding={3} margin='auto' marginTop={5} borderRadius={5} maxWidth={400}>
+          <Typography padding={3} textAlign="center" variant="h2">Login</Typography>
+          <TextField margin="normal" placeholder='Name'/>
+          <TextField margin="normal" placeholder='Email' type={'email'}/>
+          <TextField margin="normal" placeholder='Password' type={'password'}/>
+          <Button sx={{borderRadius:3, marginTop:3}} variant="contained" color='warning' >Submit</Button>
+          <Button sx={{borderRadius:3, marginTop:3}} >Change to Signup</Button>
+        </Box>
+      </form>
+    </div>
   )
 }
 
