72a5111f91e993d5704df8ef4e5edd8475907da7
Ryan McGowan <vitomaserati@gmail.com>
2023-01-10 20:19:21 -0800

    add admin post page and start styling

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,89 +1,32 @@
-import React,{useState} from 'react';
-import { Typography, Box, InputLabel, TextField, Button, FormControlLabel,RadioGroup,Radio} from "@mui/material";
-import { Link,useNavigate } from "react-router-dom";
-import { useStyles } from "./utils";
-import axios from "axios";
-const labelStyles = { mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" };
+import React from 'react'
+import {Box, InputLabel, TextField, Typography} from '@mui/material';
 
+const labelStyles={mb:1,mt:2,fontSize:'24px', fontWeight:'bold'}
 
-//inputs, fetch/pull through axios
-function Admin() {
-  const classes = useStyles();
-  const navigate = useNavigate();
-  const [inputs, setInputs] = useState({
-    title: "",
-    name: "",
-    imageURL: "",
-    post: "",
-    sport: "other",
-
-    
-  });
-  
-  //Form 
-  return (
-    <div>
-      <form onSubmit>
-        <Box
-          display="flex"
-          flexDirection={'column'}
-          width={"80%"} 
-          border={3}
-          borderRadius={10}
-          borderColor="black"
-          boxShadow="10px 10px 110px black"
-          margin={'auto'}
-          padding={3}
-          marginTop={5}>
-          <Typography
-            textAlign={'center'}
-            fontWeight={'bold'}
-          >CREATE NEW POST
-          </Typography>
-
-          <InputLabel className={classes.font} sx={labelStyles}>Title</InputLabel>
-          <TextField 
-          className={classes.font}
-          name="title"
-          
-          value={inputs.title}
-          variant="outlined"/>
-          <InputLabel className={classes.font} sx={labelStyles}>Name</InputLabel>
-          <TextField 
-          className={classes.font}
-          name="name"
-          
-          value={inputs.name}
-          variant="outlined"/>
-          <InputLabel className={classes.font} sx={labelStyles}>ImageURL</InputLabel>
-          <TextField
-           className={classes.font}
-           name="imageURL"
-           
-           value={inputs.imageURL}
-           variant="outlined" />
-           <InputLabel className={classes.font} sx={labelStyles}>
-           Post
-          </InputLabel>
-          <TextField
-            className={classes.font}
-            name="post"
-            
-            value={inputs.post}
-            variant="outlined"
-            multiline={true}
-           />
-           
-           <Button type="submit" variant='contained' sx={{ margin: 1, borderRadius: 4 }} color="warning">Create Post</Button>
-
-           
-       
-
-          
+const Admin = () => {
+  return (<div>
+      <form>
+        <Box border={3} 
+        borderColor="pink" 
+        borderRadius={10}
+        boxShadow="10px 10px 20px #ccc" 
+        padding={3}
+        margin={'auto'} 
+        marginTop={10}
+        display='flex' 
+        flexDirection={'column'} 
+        width={"80%"}>
+          <Typography fontWeight={'bold'} padding={3}color="red" variant="h2" textAlign={'center'}>Add a new Trip</Typography>
+          <InputLabel sx={labelStyles}>Trip Title</InputLabel>
+          <TextField/>
+          <InputLabel sx={labelStyles}>Description</InputLabel>
+          <TextField/>
+          <InputLabel sx={labelStyles}>ImageURL</InputLabel>
+          <TextField/>
         </Box>
       </form>
     </div>
   )
-};
+}
 
 export default Admin
