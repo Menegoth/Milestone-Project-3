bd87850b1d6a36ad812fca0d520fc2f2d6aad8fe
Ryan McGowan <vitomaserati@gmail.com>
2023-01-12 21:31:35 -0800

    add state to auth page for signup and change to buttons

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,8 +1,22 @@
+import { Button, TextField, Typography, Box } from '@mui/material'
 import React from 'react'
+import { useState } from 'react'
 
 const Auth = () => {
+  const [isSignup, setIsSignup] = useState(false)
   return (
-    <div>Auth</div>
+    <div>
+      <form>
+        <Box maxWidth={400} display="flex" flexDirection={"column"} alignItems="center" justifyContent={"center"} boxShadow="15px 15px 25px #ccc" padding={3} margin='auto' marginTop={5} borderRadius={5}>
+          <Typography variant="h2" padding={3} textAlign="center">{isSignup ? "Signup" : "Login"}</Typography>
+          { isSignup && <TextField placeholder="Name" margin="normal"/>}
+          <TextField type={"email"} placeholder="Email" margin="normal"/>
+          <TextField type={"password"} placeholder="Password" margin="normal"/>
+          <Button variant='contained' sx={{borderRadius:3, marginTop:1}} color="warning">Submit</Button>
+          <Button onClick={()=>setIsSignup(!isSignup)} sx={{borderRadius:3, marginTop:1}} >Change to {isSignup ? "Login" : "Signup"}</Button>
+        </Box>
+      </form>
+    </div>
   )
 }
 
